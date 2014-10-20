787d0e2c78dd5f75e4951495d384a0472946a7ba
Vernon Zheng <kevonzheng@gmail.com>
2014-10-20 11:13:40 +0800

    add sub demo: generate sql by excel/txt(android permission info)

--- ApkPermissionGenerateSql/permission_main.py
+++ ApkPermissionGenerateSql/permission_main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-根据android应用权限txt，持久化到数据库表
+根据android应用权限txt，生成sql,持久化到数据库表
 
 """
 
@@ -10,7 +10,7 @@ __author__ = 'vernonzheng'
 
 def main():
     print 'start..'
-    read_permission_txt()
+    read_permission_from_excel_txt()
     print 'end..'
 
 
@@ -22,7 +22,7 @@ def read_permission_txt():
             while 1:
                 lines = f.readlines()
                 if flag == 1:
-                    break
+                    return
                 for i in range(0, len(lines)-1):
                     line = lines[i]
                     if str(line).strip().find('---end---') != -1:
@@ -36,8 +36,19 @@ def read_permission_txt():
                         permission_info_ch = str(lines[i]).replace('android.permission.', '').replace('\r\n', '').strip()
                         i += 1
                         permission_info_en = str(lines[i]).replace('android.permission.', '').replace('\r\n', '').strip()
-                        print(sql % (permission_name, permission_info_ch, permission_info_en)+"\r")
+                        sql_f.write(sql % (permission_name, permission_info_ch, permission_info_en)+"\r")
 
+
+#根据excel复制出的txt，分隔符为/t
+def read_permission_from_excel_txt():
+    sql = "insert into mkt_app_permission_meta values(%s,'%s','','');"
+    with open('permission_new.txt', 'r') as f:
+        with open('permission_new.sql', 'w') as sql_f:
+            for line in f.readlines():
+                if str(line).strip() == '':
+                    break
+                items = str(line).split('\t')
+                sql_f.write(sql % (items[0], items[1]) + "\r")
 
 if __name__ == '__main__':
     main()
